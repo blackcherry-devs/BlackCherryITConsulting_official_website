22485a0dd60019fd0f226de6a07311cb2b84150b
Omar <omarchavezgutierrezdevelasco@gmail.com>
2026-05-15 10:17:29 -0600

    cambios para mejorar perfomance, SEO, pixeles

--- components/home/TechStack.tsx
+++ components/home/TechStack.tsx
@@ -34,13 +34,13 @@ export default function TechStack() {
         </div>
 
         {/* Right Side: Staggered Carousels */}
-        <div className="lg:w-[65%] absolute lg:relative inset-0 lg:inset-auto z-0 lg:z-10 flex gap-4 lg:gap-8 items-start h-full lg:h-[700px] overflow-hidden px-[var(--gutter-width)] lg:px-0 lg:pr-4 py-10 lg:py-20 w-full">
+        <div className="lg:w-[65%] absolute lg:relative inset-0 lg:inset-auto z-0 lg:z-10 flex gap-4 lg:gap-8 items-start h-full lg:h-[700px] overflow-hidden px-[var(--gutter-width)] lg:px-0 lg:pr-4 py-10 lg:py-20 w-full justify-center min-[600px]:justify-start">
           {/* Carousel 1 (Scrolls UP) */}
-          <div className="flex-1 lg:flex-[1.4] relative h-full lg:h-[600px] overflow-hidden bg-transparent lg:-mt-20">
-            <div className="flex flex-col gap-0 py-0 animate-scroll-up">
+          <div className="flex-1 lg:flex-[1.4] relative h-full lg:h-[600px] overflow-hidden bg-transparent lg:-mt-20 flex flex-col items-center min-[600px]:items-start">
+            <div className="flex flex-col gap-0 py-0 animate-scroll-up w-full items-center min-[600px]:items-start">
               {/* Render twice for seamless loop */}
               {[...stackTop, ...stackTop].map((tech, i) => (
-                <div key={i} className="h-[100px] lg:h-[150px] flex items-center justify-start">
+                <div key={i} className="h-[100px] lg:h-[150px] flex items-center justify-center min-[600px]:justify-start w-full">
                   <span className="font-headline font-black text-5xl lg:text-7xl tracking-tighter uppercase opacity-20 hover:opacity-100 hover:text-primary-container transition-all cursor-default whitespace-nowrap">
                     {tech}
                   </span>
@@ -49,8 +49,8 @@ export default function TechStack() {
             </div>
           </div>
 
-          {/* Carousel 2 (Scrolls DOWN) */}
-          <div className="flex-1 relative h-full lg:h-[600px] overflow-hidden bg-transparent lg:mt-20">
+          {/* Carousel 2 (Scrolls DOWN) - Hidden on mobile < 600px */}
+          <div className="flex-1 relative h-full lg:h-[600px] overflow-hidden bg-transparent lg:mt-20 hidden min-[600px]:block">
             <div className="flex flex-col gap-0 py-0 animate-scroll-down">
               {/* Render twice for seamless loop */}
               {[...stackBottom, ...stackBottom].map((tech, i) => (
