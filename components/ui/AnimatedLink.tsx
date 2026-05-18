059a1744d1b1bba8f748bd8bc2d531727a32a495
Diego Hernandez <diego_hernandez@epam.com>
2026-05-17 18:53:33 -0600

    refactor: wrap AnimatedLink in forwardRef to support ref forwarding

--- components/ui/AnimatedLink.tsx
+++ components/ui/AnimatedLink.tsx
@@ -11,39 +11,45 @@ interface AnimatedLinkProps extends Omit<React.AnchorHTMLAttributes<HTMLAnchorEl
   href: string; 
 }
 
-export default function AnimatedLink({ href, children, className, onClick, target, ...props }: AnimatedLinkProps) {
-  const { triggerTransition } = usePageTransition();
-  const pathname = usePathname();
+const AnimatedLink = React.forwardRef<HTMLAnchorElement, AnimatedLinkProps>(
+  ({ href, children, className, onClick, target, ...props }, ref) => {
+    const { triggerTransition } = usePageTransition();
+    const pathname = usePathname();
 
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    // If there's an existing onClick, fire it
-    if (onClick) {
-      onClick(e);
-    }
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      // If there's an existing onClick, fire it
+      if (onClick) {
+        onClick(e);
+      }
 
-    // Don't intercept if user opens in new tab (ctrl/meta/shift click) or has target="_blank"
-    if (e.ctrlKey || e.metaKey || e.shiftKey || e.altKey || target === '_blank') {
-      return;
-    }
-
-    const hrefString = href.toString();
-    const isInternal = hrefString.startsWith('/') || hrefString.startsWith('#');
-    
-    // Don't animate if we are only navigating to a hash on the exact same page
-    const [path] = hrefString.split('#');
-    if (path === pathname || path === '') {
+      // Don't intercept if user opens in new tab (ctrl/meta/shift click) or has target="_blank"
+      if (e.ctrlKey || e.metaKey || e.shiftKey || e.altKey || target === '_blank') {
         return;
-    }
+      }
 
-    if (isInternal) {
-      e.preventDefault();
-      triggerTransition(hrefString, e.clientX, e.clientY);
-    }
-  };
+      const hrefString = href.toString();
+      const isInternal = hrefString.startsWith('/') || hrefString.startsWith('#');
+      
+      // Don't animate if we are only navigating to a hash on the exact same page
+      const [path] = hrefString.split('#');
+      if (path === pathname || path === '') {
+          return;
+      }
 
-  return (
-    <Link href={href} className={className} onClick={handleClick} target={target} {...props}>
-      {children}
-    </Link>
-  );
-}
+      if (isInternal) {
+        e.preventDefault();
+        triggerTransition(hrefString, e.clientX, e.clientY);
+      }
+    };
+
+    return (
+      <Link ref={ref} href={href} className={className} onClick={handleClick} target={target} {...props}>
+        {children}
+      </Link>
+    );
+  }
+);
+
+AnimatedLink.displayName = 'AnimatedLink';
+
+export default AnimatedLink;
